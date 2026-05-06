936f6e30b7a6f202b7b84cedfd6ff2a4b8eb33d6
Sehee Chang <sxc240036@utdallas.edu>
2026-05-06 03:56:44 -0500

    basic object duplication and random translation setup

--- ChangSehee_P02_code.py
+++ ChangSehee_P02_code.py
@@ -8,3 +8,9 @@ def simple_scatter():
         return
     
     source_obj = selection[0]
+
+    for i in range(3):
+        new_obj = cmds.duplicate(source_obj)[0]
+        x = random.uniform(-7,7)
+        y = random.uniform(-7,7)
+        cmds.xform(new_obj, translation=[x, 0, z])
